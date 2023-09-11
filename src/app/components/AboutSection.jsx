4db6350789e8658c09b189cb47f41229b5608333
Dionel Bawag <bawagdionel@gmail.com>
2023-09-12 02:02:09 +0800

    day 1

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,102 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>JavaScript</li>
+        <li>Typescript</li>
+        <li>Next.js</li>
+        <li>React.js</li>
+        <li>Node.js</li>
+        <li>shadcn.ui</li>
+        <li>prisma.io</li>
+        <li>cloudinary</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>New Era University</li>
+        <li>AMA Computer College</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Hobbies",
+    id: "hobbies",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Reading Self Help Books</li>
+        <li>Watching Documentaries</li>
+        <li>Fish Keeping</li>
+        <li>Music Enthusiasts</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.png" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+          My passion lies in crafting dynamic projects that seamlessly blend creativity and functionality. Proficient in JavaScript and TypeScript, I am particularly enthusiastic about utilizing Next.js to bring modern web applications to life. I also specialize in creating user interfaces using shadcn.ui and adeptness in managing data with Prisma and MySQL. 
+          I'm excited to launch my career as a Software Engineer, 
+          bringing fresh ideas and a dedication to continuous growth. 
+          I'm actively seeking opportunities to contribute my skills and learn from experienced professionals in the field.
+Thank you for visiting my profile!
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("hobbies")}
+              active={tab === "hobbies"}
+            >
+              {" "}
+              Hobbies{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
---
+++ src/app/components/ProjectsSection.jsx
@@ -0,0 +1,182 @@
+"use client";
+import React, { useState, useRef } from "react";
+import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
+import { motion, useInView } from "framer-motion";
+
+const projectsData = [
+  {
+    id: 1,
+    title: "Soon",
+    description: "Project 1 description",
+    image: "/images/projects/1.png",
+    tag: ["All", "Web Development"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 2,
+    title: "Soon",
+    description: "Project 2 description",
+    image: "/images/projects/2.png",
+    tag: ["All", "Web Development"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 3,
+    title: "Soon",
+    description: "Project 3 description",
+    image: "/images/projects/3.png",
+    tag: ["All", "Web Development"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 4,
+    title: "Soon",
+    description: "Project 4 description",
+    image: "/images/projects/4.png",
+    tag: ["All", "Web Development"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 5,
+    title: "Soon",
+    description: "Project 5 description",
+    image: "/images/projects/5.png",
+    tag: ["All", "Web Development"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 6,
+    title: "Soon",
+    description: "Project 5 description",
+    image: "/images/projects/6.png",
+    tag: ["All", "Web Development"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 7,
+    title: "Udemy jQuery",
+    description: "My Udemy HTML certification encompasses all essential HTML topics, providing me with a strong foundation in web development.",
+    image: "/images/projects/cert1.jpg",
+    tag: ["All", "Certifications"],
+    gitUrl: "/",
+    previewUrl: "https://www.udemy.com/certificate/UC-8ce0487d-50d2-42d3-b668-1faaa36c426f/",
+  },
+  {
+    id: 8,
+    title: "Udemy Git Bash",
+    description: "My Udemy HTML certification encompasses all essential HTML topics, providing me with a strong foundation in web development.",
+    image: "/images/projects/cert2.jpg",
+    tag: ["All", "Certifications"],
+    gitUrl: "/",
+    previewUrl: "https://www.udemy.com/certificate/UC-90017438-bb85-4d4a-8248-e9a39bde7a4c/",
+  },
+  {
+    id: 9,
+    title: "Udemy jQuery",
+    description: "My Udemy HTML certification encompasses all essential HTML topics, providing me with a strong foundation in web development.",
+    image: "/images/projects/cert3.jpg",
+    tag: ["All", "Certifications"],
+    gitUrl: "/",
+    previewUrl: "https://www.udemy.com/certificate/UC-bbf48633-6e99-43de-96be-6bf6e64f15aa/",
+  },
+  {
+    id: 10,
+    title: "Udemy Javascript",
+    description: "My Udemy HTML certification encompasses all essential HTML topics, providing me with a strong foundation in web development.",
+    image: "/images/projects/cert4.jpg",
+    tag: ["All", "Certifications"],
+    gitUrl: "/",
+    previewUrl: "https://www.udemy.com/certificate/UC-de4a614a-bef6-4beb-9b34-c19344bbf955/",
+  },
+  {
+    id: 11,
+    title: "Udemy cPanel",
+    description: "My Udemy HTML certification encompasses all essential HTML topics, providing me with a strong foundation in web development.",
+    image: "/images/projects/cert5.jpg",
+    tag: ["All", "Certifications"],
+    gitUrl: "/",
+    previewUrl: "https://www.udemy.com/certificate/UC-eedd2d0e-e64a-4d9c-b1e7-862a8417eebe/",
+  },
+  {
+    id: 12,
+    title: "Udemy HTML",
+    description: "My Udemy HTML certification encompasses all essential HTML topics, providing me with a strong foundation in web development.",
+    image: "/images/projects/cert6.jpg",
+    tag: ["All", "Certifications"],
+    gitUrl: "/",
+    previewUrl: "https://www.udemy.com/certificate/UC-efeb0e74-4f36-451d-9484-9ca69bb7218b/",
+  },
+];
+
+const ProjectsSection = () => {
+  const [tag, setTag] = useState("All");
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectsData.filter((project) =>
+    project.tag.includes(tag)
+  );
+
+  const cardVariants = {
+    initial: { y: 50, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+  };
+
+  return (
+    <section id="projects">
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+        My Projects
+      </h2>
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+        <ProjectTag
+          onClick={handleTagChange}
+          name="All"
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Web Development"
+          isSelected={tag === "Web Development"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Certifications"
+          isSelected={tag === "Certifications"}
+        />
+      </div>
+      <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {filteredProjects.map((project, index) => (
+          <motion.li
+            key={index}
+            variants={cardVariants}
+            initial="initial"
+            animate={isInView ? "animate" : "initial"}
+            transition={{ duration: 0.3, delay: index * 0.4 }}
+          >
+            <ProjectCard
+              key={project.id}
+              title={project.title}
+              description={project.description}
+              imgUrl={project.image}
+              gitUrl={project.gitUrl}
+              previewUrl={project.previewUrl}
+            />
+          </motion.li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default ProjectsSection;
